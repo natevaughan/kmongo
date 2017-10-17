c48400a669fc978b888e8c199d44eaab9417bd6c
Nate Vaughan <nvaughanguitar@yahoo.com>
2017-10-17 17:10:31 -0500

    added dummy test

---
+++ src/test/kotlin/com/natevaughan/kmongo/DummyTest.kt
@@ -0,0 +1,19 @@
+package com.natevaughan.kmongo
+
+import org.junit.Test
+import org.junit.Assert.*
+import org.slf4j.LoggerFactory
+
+/**
+ * Created by nate on 10/13/17
+ */
+class DummyTest {
+    companion object {
+        val log = LoggerFactory.getLogger(DummyTest::class.java)
+    }
+
+    @Test
+    fun dummyTest() {
+        assertTrue(true)
+    }
+}
